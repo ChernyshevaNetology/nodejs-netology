1720663496ffafd89bc67ff51be72792f59b8113
ChernyshevaNetology <chernyshevak87@mail.ru>
2025-02-12 11:54:25 +0200

    read static file

--- http/index.js
+++ http/index.js
@@ -1,21 +1,27 @@
 import http from "http";
 import url from "url";
+import { readFileSync } from "fs";
 
 console.log("creating a server!");
 
 const server = http.createServer((req, res) => {
   const urlParsed = url.parse(req.url, true);
-  console.log("urlParsed", urlParsed);
+  console.log("res", req.url);
   const { pathname, query } = urlParsed;
   console.log(pathname, query);
   const { method } = req;
   console.log("method", method);
+
+  const homePage = readFileSync("./templates/homePage.html");
 
   res.setHeader("Content-Type", "text/html; charset=utf-8;");
 
   if (pathname === "/" || pathname === "/index") {
     res.write(`<h2>Рендерим html с сервера!</h2>`);
     res.end(`<h2>Закончили</h2>`);
+  } else if (req.url === "/home") {
+    res.write(homePage);
+    res.end();
   } else if (pathname === "/order") {
     res.write(`<h2>Рендерим Страницу ордеров!</h2>`);
     res.end();
@@ -23,7 +29,10 @@ const server = http.createServer((req, res) => {
     res.write(`<h2>Рендерим страницу плагинов</h2>`);
     res.end();
   } else if (pathname === "/json") {
-    res.writeHead(200, { "Content-Type": "application/json" });
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    }); // Allow all origins});
     const data = [
       { id: 1, name: "Andrey" },
       { id: "2", name: "Alexander" },
