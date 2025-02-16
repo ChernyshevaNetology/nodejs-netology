876c8e9027ebc8dc4367478f327776f6e9a5af06
ChernyshevaNetology <chernyshevak87@mail.ru>
2025-02-16 11:48:09 +0200

    add config.js file

---
+++ config.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
+
+const config = {
+  dbHost: process.env.PORT || "8000",
+  api_key: process.env.API_KEY || "b7cfc9f3d468c6c050665204449e8e99",
+};
+
+export default config;
